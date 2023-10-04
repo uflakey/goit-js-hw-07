9f14d96ce29a735105d4ed1618d086f91e2b5083
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-05 00:28:15 +0300

    task1-finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const containerGalary = document.querySelector(".gallery");
+
+containerGalary.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+containerGalary.addEventListener("click", handlerclick);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+ <a class="gallery__link" href="${original}">
+    <img
+     class="gallery__image"
+     src="${preview}"
+      data-source="${original}"
+     alt="${description}"
+   />
+   </a>
+ </li>`
+    )
+    .join("");
+}
+
+function handlerclick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const modal = basicLightbox.create(`
+	<div class="modal">
+  <img src="${event.target.dataset.source}"width="800" height="600" alt=""/>
+</div> 
+`);
+  modal.show();
+
+  containerGalary.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      modal.close();
+    }
+  });
+}
