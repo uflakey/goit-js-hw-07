77dc2005566c843aaf43fc08f7d0865cc45c69a5
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-05 16:40:24 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const containerGalary = document.querySelector(".gallery");
+
+containerGalary.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
+
+const modal = new SimpleLightbox(".gallery a", {});
